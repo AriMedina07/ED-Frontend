ea6c122663c84e965c1a6716349570ffa86decc6
Diego Gutiérrez <diegogutcat28@gmail.com>
2023-01-31 12:13:56 -0500

    Se configuró eslint y prettier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+   parser: '@typescript-eslint/parser',
+   plugins: ['@typescript-eslint'],
+   extends: ['prettier'],
+   rules: {
+      quotes: ['error', 'single'],
+      indent: ['error', 3],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-unused-vars': [
+         'error',
+         { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'object-curly-spacing': ['error', 'always'],
+      'no-console': 'warn',
+   },
+};
