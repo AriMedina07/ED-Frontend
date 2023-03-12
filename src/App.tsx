38cfaec16f24d8bc11a250da048caf54b0cb83e0
Diego Gutiérrez <diegogutcat28@gmail.com>
2023-03-12 18:16:38 -0500

    Se añadió el routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/global.scss';
 
 function App() {
-   return <div className="App" />;
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<div>Inicio</div>} />
+            <Route
+               path="/estudiante"
+               element={<div>Hola soy un estudiante</div>}
+            />
+            <Route
+               path="/coordinador"
+               element={<div>Hola soy el coordi</div>}
+            />
+            <Route
+               path="/administrador"
+               element={<div>Hola soy el admin</div>}
+            />
+            <Route path="*" element={<div>404 not found</div>} />
+         </Routes>
+      </BrowserRouter>
+   );
 }
 
 export default App;
