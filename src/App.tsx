159b5908a3b024da1f9f4bc18060d1f31efbd23b
Pastelin <lealpastelin@gmail.com>
2023-03-12 18:52:34 -0500

    Login page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './styles/global.scss';
+import Login from './components/shared/login/index';
 
 function App() {
-   return <div className="App" />;
+   return (
+      <div className="App">
+         <Login />
+      </div>
+   );
 }
 
 export default App;
